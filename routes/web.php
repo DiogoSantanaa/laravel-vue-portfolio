50347052c6bfe3a41168fbef495567cd721c7a30
Diogo Santana <diogo.santana@webteam.pt>
2020-02-12 09:59:27 +0000

    Admin folder create and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +20,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'ProjectsController@index')->name('admin.index');
