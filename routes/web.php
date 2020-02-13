844918988394bb696dac6ae534c4d36ba2a288f6
Diogo Santana <diogo.santana@webteam.pt>
2020-02-13 10:22:19 +0000

    Views created and some styling added

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
-Route::get('/admin', 'ProjectsController@index')->name('admin.index');
+Route::get('/projects', 'ProjectsController@index')->name('projects.index');
+Route::post('/projects', 'ProjectsController@store');
+Route::get('/projects/create', 'ProjectsController@create');
+Route::get('/projects/{project}', 'ProjectsController@show')->name('projects.show');
+Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+Route::put('/projects/{project}', 'ProjectsController@update');
